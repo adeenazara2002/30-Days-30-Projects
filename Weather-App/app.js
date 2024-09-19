baefeb9c964a77b7d5c0fdc80e3f16efae67eeb4
adeenazara2002 <adinazara2001@gmail.com>
2024-09-19 20:56:52 +0500

    Weather App

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -2,3 +2,24 @@ const container = document.querySelector('.container');
 const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
+
+search.addEventListener('click' , () =>{
+  const APIKey = '1a7e7e1e03ebdab6acd6fc254f7f60a5';
+  const city = document.querySelector('.search-box input').value;
+
+  if (city == '') {
+    return;
+  }
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&
+    appid=${APIKey}`).then(response => response.json()).then(json =>{
+        const image = document.querySelector('.weather-box img');
+        const temperature = document.querySelector('.weather-box .temperature');
+        const description = document.querySelector('.weather-box .description');
+        const humidity = document.querySelector('.weather-details .humidity span');
+        const wind = document.querySelector('.weather-details .wind span');
+
+
+
+    });
+});
