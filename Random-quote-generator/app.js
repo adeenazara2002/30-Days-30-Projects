bc4750e3ea337ef1939863de3029b9f3d6024706
adeenazara2002 <adinazara2001@gmail.com>
2024-09-27 00:36:12 +0500

    Random quote generator

--- Random-quote-generator/app.js
+++ Random-quote-generator/app.js
@@ -1,12 +1,41 @@
 const quoteText = document.querySelector('.quote'),
-quoteBtn = document.querySelector('button');
+authorName =document.querySelector(".author .name"),
+quoteBtn = document.querySelector('button'),
+soundBtn = document.querySelector(".sound"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter");
+
+
 
 function randomQuote() {
-    // console.log("Clicked")
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading Quote....";
     fetch("https://api.quotable.io/random").then(res => 
         res.json()).then(result => {
-            
+        //  console.log(result)  
+         quoteText.innerText = result.content;
+         authorName.innerText = result.author;
+         quoteBtn.innerText = "New Quote";
+         quoteBtn.classList.remove("loading");
+
+
         })
 }
 
-quoteBtn.addEventListener('click' , randomQuote)
+soundBtn.addEventListener("click", ()=>{
+    let utterance = new SpeechSynthesisUtterance(
+       `${quoteText.innerText} by ${authorName.innerText}` 
+    );
+    speechSynthesis.speak(utterance);
+});
+
+copyBtn.addEventListener("click", ()=>{
+  navigator.clipboard.writeText(quoteText.innerText);
+});
+
+twitterBtn.addEventListener("click", ()=>{
+   let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+   window.open(tweetUrl , "_blank");
+
+  });
+quoteBtn.addEventListener('click' , randomQuote);
